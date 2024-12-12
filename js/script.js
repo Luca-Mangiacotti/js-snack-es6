ff3d67861d63a3f2f92f946837017fa29e88d917
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2024-12-12 16:30:07 +0100

    finish 2nd snack

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ for (let i = 0; i < bikes.length ; i++){
         //ALTRIMENTI SE il peso della bici è uguale a quello della variabile di controllo
         //aggiungi il nome della bici al risultato delle bici con peso minore
     } else if ( bikes[i].weight === weightCheck ) {
-        result += ` , ${bikes[i].name}`
+        result += ` ,${bikes[i].name}`
     }
     
 }
@@ -67,6 +67,89 @@ console.log(weightCheck, result)
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+//array contenente le squadre di calcio
+const footballTeams = [
+    {
+        teamName : "Sampdoria",
+        points : 0,
+        fouls : 0
+    },
+    {
+        teamName : "Albinoleffe",
+        points : 0,
+        fouls : 0
+    },
+    {
+        teamName : "Real Madrid",
+        points : 0,
+        fouls : 0
+    },
+    {
+        teamName : "Pisa",
+        points : 0,
+        fouls : 0
+    },
+    {
+        teamName : "Foggia",
+        points : 0,
+        fouls : 0
+    }
+]
+
+//dichiarazione delle variabili d'appoggio per i valori da restituire e l'array da restituire in output
+let currentPoints = 0
+let currentFouls = 0
+let currentName = ""  
+const outResult = []
+
+//dichiarazione della funzione per ottenere i numeri randomici
+/** 
+    * Get Random Integer between a range
+    * @param {number} min
+    * @param {number} max
+    * @returns {number}
+*/ 
+function getRandomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min
+}
+
+//impostazione del ciclo per ottenere i valori di "points" e "fouls" delle squadre
+
+for (let i = 0 ; i < footballTeams.length; i++){
+
+    //creazione di un oggetto d'appoggio da pushare nell'array di output
+    const currentTeam =  {   
+        nameSet:"",
+        foulsDone : 0
+    }
+
+    //diamo il nome della squadra in posizione [i] all'array di output
+    currentName = footballTeams[i].teamName
+    currentTeam.nameSet = currentName
+    
+
+    //qui viene richiamata la funzione per generare i punti per la squadra in modo casuale (da 0 a 70 punti)
+    currentPoints = getRandomInt(0, 70) 
+    footballTeams[i].points = currentPoints
+    
+    //qui viene richiamata la funzione per generare i falli per la squadra in modo casuale (da 0 a 30)
+    currentFouls = getRandomInt(0, 30) 
+    footballTeams[i].fouls = currentFouls
+    //diamo i falli della squadra in posizione [i] all'array di output
+    currentTeam.foulsDone = currentFouls
+
+    outResult.push(currentTeam)
+
+}
+
+//OUTPUT risultato
+
+console.log(outResult)
+
+
+
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
